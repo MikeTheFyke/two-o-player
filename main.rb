46e22dec07222083e6e2c6d151491c42914b56a8
vagrant <mikefyke@hotmail.com>
2019-05-16 16:29:34 +0000

    Used the gets.chomp method to grab user input

--- main.rb
+++ main.rb
@@ -1,4 +1,13 @@
-require_relative '.game.rb'
-require_relative '.player.rb'
-require_relative '.questions.rb'
-require_relative '.turn.rb'
+# 2. Define what files main will require
+require '.pry'
+require_relative './game.rb'
+require_relative './player.rb'
+require_relative './questions.rb'
+require_relative './turn.rb'
+
+# Creates 2 new players
+player1 = Player.new(1)
+player2 = Player.new(2)
+
+# Creates a new game instance
+Game.new(5, player1, player2).start
---
+++ turn.rb
@@ -0,0 +1,20 @@
+class Turn
+
+  def initialize(player)
+
+    new_question = Question.new
+    puts
+    p "--- New Turn---"
+    p "player #{player.player_number}: #{new_question.question}"
+    puts "Enter Answer : "
+    # Stores a players answer with gets.chomp
+    answer = gets.chomp
+    result = new_question.correct?(answer.to_i)
+    player.update_lives(result)
+    if result == true
+        p "player #{player.player_number}: That Is The Correct Answer! "
+    else
+        p "player #{player.player_number}: That is Unfortunately Wrong "
+    end
+  end
+end
